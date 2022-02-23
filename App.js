c51304e05a2512ddec7e24232dd1f9797e9e58de
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2022-02-23 23:59:26 +0000

    added a custom google font as the default font

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 // add css style for android safearea view
+
+// importing google fonts
+import { 
+  Righteous_400Regular 
+} from '@expo-google-fonts/righteous';
+
 export default function App() {
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Fitness App</Text>
+      <Text style={styles.text}>Fit
+        <Text styles={styles.blueText}>Me</Text>
+        <Image source={require("./assets/barbell.png")}/>
+      </Text>
+
+
     </SafeAreaView>
   );
 }
@@ -12,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 25,
+    fontFamily: 'Righteous',
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
+  
+    // justifyContent: 'center',
   },
+
+  text: {
+    color: '#424242',
+    fontFamily: 'Righteous_400Regular',
+  },
+
+  blueText: {
+    color: 'yellow',
+  },
+
 });
