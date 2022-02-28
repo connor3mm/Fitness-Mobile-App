1f343f11154808f1167322a1304844437af7f91e
Ramsey Ediku CS2019 <ramsey.ediku.2020@uni.strath.ac.uk>
2022-02-28 13:54:55 +0000

    created the home page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, Alert } from 'react-native';
 // add css style for android safearea view
 
 // importing google fonts
@@ -7,17 +7,31 @@ import {
   Righteous_400Regular 
 } from '@expo-google-fonts/righteous';
 
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
+  let [fontsLoaded, error] = useFonts ({
+    Righteous_400Regular,
+  });
 
-
+  if (!fontsLoaded) return <AppLoading/>
+  
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Fit
-        <Text styles={styles.blueText}>Me</Text>
-        <Image source={require("./assets/barbell.png")}/>
-      </Text>
+      <View style={{ flex: 2.5, justifyContent: 'center'}}>
+        <Text style={styles.text}>Welcome to Fit<Text style={styles.blueText}>Me</Text></Text>
+        <Text style={styles.smallText}>Upgrade your fitness to the next level!</Text>
+      </View>
 
-
+      <View style={{ flex: 1, justifyContent: 'flex-end', width: '80%', marginBottom: 1, }}>
+        <View style={{ width: '100%', borderColor: 'orange', }}>
+          <Button color="#4356FF" title="New! Get Started"/>
+          </View>
+        <View style={{ width: '100%', marginBottom: 75, marginTop: 15, borderColor: '#4356FF', borderWidth: 3,  color: 'white', }}>
+          <Button color="#FFFFFF" title="Existing user? Log In"/>
+          </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -25,21 +39,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 25,
-    fontFamily: 'Righteous',
     backgroundColor: 'white',
-    alignItems: 'center',
-  
-    // justifyContent: 'center',
+    alignItems: 'center',  
   },
 
   text: {
+    fontSize: 45.5,
     color: '#424242',
     fontFamily: 'Righteous_400Regular',
   },
 
-  blueText: {
-    color: 'yellow',
+  smallText: {
+    fontSize: 15.5,
+    fontFamily: 'Righteous_400Regular',
+    color: '#424242',
+    alignSelf: 'center',
+
   },
+
+  blueText: {
+    color: '#4356FF',
+  },
+
+  
 
 });
