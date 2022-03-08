2df8db57888a41d0a69f9155727c2947d150a8ac
Connor McGuire SE2019 <connor.mcguire.2020@uni.strath.ac.uk>
2022-03-08 23:43:35 +0000

    Dynamic height and weight input boxes added

--- Screens/BMICalculatorPage.js
+++ Screens/BMICalculatorPage.js
@@ -1,42 +1,75 @@
 import React, {useState} from 'react';
-import {Button, SafeAreaView,TextInput, StyleSheet, Text, View} from "react-native";
-import { RadioButton } from 'react-native-paper';
+import {Button, SafeAreaView, TextInput, StyleSheet, Text, View} from "react-native";
+import {RadioButton} from 'react-native-paper';
+import RadioGroup from 'react-native-radio-buttons-group';
 
 
+const radioButtonsData2 = [{
+    id: '1',
+    label: 'Stone',
+    value: 'stone'
+}, {
+    id: '2',
+    label: 'Pounds',
+    value: 'pounds'
+}]
 
 export default function BMICalculator() {
 
-    const [heightType, setHeightType] = useState('');
+    //height values
+    const [heightType, setHeightType] = useState('feet');
 
     const [heightFeet, setHeightFeet] = useState(0);
     const [heightInches, setHeightInches] = useState(0);
     const [heightCm, setHeightCm] = useState(0);
 
-    const [weightType, setWeightType] = useState('');
+    //weight values
+    const [weightType, setWeightType] = useState('stone');
 
     const [massStone, setMassStone] = useState(0);
     const [massStoneLbs, setMassStoneLbs] = useState(0);
     const [massLBS, setMassLBS] = useState(0);
     const [massKG, setMassKG] = useState(0);
 
+    //age values
     const [age, setAge] = useState(0);
+
+    //sex values
     const [checked, setChecked] = React.useState('');
 
+    //activity level value
     const [activity, setActivity] = React.useState('');
 
+    //BMI value
     const [bmi, setBmi] = useState(0);
 
-    const calculate = () => {
-         console.log(checked)
-         console.log(age)
-         console.log(massStone)
-         console.log(heightFeet)
-         console.log(heightInches)
-         console.log(activity)
-         console.log(heightType)
-         console.log(weightType)
 
-        const formValid = heightFeet > 0 &&  heightInches > 0 && massStone > 0;
+
+    const [radioButtons2, setRadioButtons2] = useState(radioButtonsData2)
+
+
+
+    function onPressRadioButton2(radioButtonsArray2) {
+        setRadioButtons2(radioButtonsArray2);
+        setWeightType(!weightType);
+    }
+
+
+    // let radioHandler = (status) => {
+    //     this.setState({ status });
+    // };
+
+    const calculate = () => {
+        console.log(checked)
+        console.log(age)
+        console.log(massStone)
+        console.log(heightFeet)
+        console.log(heightInches)
+        console.log(activity)
+        console.log(heightType)
+        console.log(weightType)
+
+        const formValid = heightFeet > 0 && heightInches > 0 && massStone > 0;
         if (!formValid) {
             return;
         }
@@ -55,109 +88,130 @@ export default function BMICalculator() {
         console.log(bmi)
     }
 
-    return(
-        <View style = {styles.container}>
+    return (
+        <View style={styles.container}>
 
             <RadioButton.Group  onValueChange={heightType => setHeightType(heightType)} value={heightType}>
                 <RadioButton.Item label="Feet" value="feet" />
                 <RadioButton.Item label="CM" value="cm" />
             </RadioButton.Group>
 
+
+
+
+            <Text>Enter Height: </Text>
             <View>
-            <Text>Enter Height:  </Text>
-            <TextInput
-                keyboardType = 'numeric'
-                style ={styles.input}
-                placeholder = 'Feet'
-                onChangeText = {(height) => setHeightFeet(height)}
-            />
 
-            <TextInput
-                keyboardType = 'numeric'
-                style ={styles.input}
-                placeholder = 'Inches'
-                onChangeText = {(height2) => setHeightInches(height2)}
-            />
+                {heightType === 'feet' ? (
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        placeholder='Feet'
+                        onChangeText={(height) => setHeightFeet(height)}
+                    />) : null}
+
+                {heightType  === 'feet' ? (
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        placeholder='Inches'
+                        onChangeText={(height2) => setHeightInches(height2)}
+                    />) : null}
+
             </View>
 
-
-            <View style = {styles.emptyDisplay}>
-                <TextInput
-                    keyboardType = 'numeric'
-                    style ={styles.input}
-                    placeholder = 'Centimeters '
-                    onChangeText = {(height) => setHeightCm(height)}
-                />
-            </View>
-
-
-            <RadioButton.Group  onValueChange={weightType => setWeightType(weightType)} value={weightType}>
-                <RadioButton.Item label="Stone" value="stone" />
-                <RadioButton.Item label="Pounds" value="LBS" />
-                <RadioButton.Item label="Kilograms" value="KG" />
-            </RadioButton.Group>
 
             <View>
-            <Text>Enter Weight:  </Text>
-            <TextInput
-                keyboardType = 'numeric'
-                style ={styles.input}
-                placeholder = 'Stone'
-                onChangeText = {(Weight) => setMassStone(Weight)}
-            />
+                {heightType === 'cm' ? (
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        placeholder='Centimeters '
+                        onChangeText={(height) => setHeightCm(height)}
+                    />) : null}
 
-            <TextInput
-                keyboardType = 'numeric'
-                style ={styles.input}
-                placeholder = 'Pounds'
-                onChangeText = {(Weight2) => setMassStoneLbs(Weight2)}
-            />
-            </View>
-
-            <View style = {styles.emptyDisplay}>
-                <TextInput
-                    keyboardType = 'numeric'
-                    style ={styles.input}
-                    placeholder = 'Pounds'
-                    onChangeText = {(Weight) => setMassLBS(Weight)}
-                />
-            </View>
-
-            <View style = {styles.emptyDisplay}>
-                <TextInput
-                    keyboardType = 'numeric'
-                    style ={styles.input}
-                    placeholder = 'Kilograms'
-                    onChangeText = {(Weight) => setMassKG(Weight)}
-                />
             </View>
 
 
-            <Text>Enter Age:  </Text>
-            <TextInput
-                keyboardType = 'numeric'
-                style ={styles.input}
-                placeholder = 'Age'
-                onChangeText = {(Age) => setAge(Age)}
-            />
-
-            <View style = {styles.radio}>
-            <Text>Choose your Sex:  </Text>
-
-            <RadioButton.Group  onValueChange={checked => setChecked(checked)} value={checked}>
-                <RadioButton.Item label="Male" value="first" />
-                <RadioButton.Item label="Female" value="second" />
+            <RadioButton.Group onValueChange={weightType => setWeightType(weightType)} value={weightType}>
+                <RadioButton.Item label="Stone" value="stone"/>
+                <RadioButton.Item label="Pounds" value="LBS"/>
+                <RadioButton.Item label="Kilograms" value="KG"/>
             </RadioButton.Group>
+
+
+            {/*<RadioGroup*/}
+            {/*    radioButtons={radioButtons2}*/}
+            {/*    onPress={onPressRadioButton2}*/}
+            {/*/>*/}
+
+            <Text>Enter Weight: </Text>
+            <View>
+                {(weightType === 'stone' )? (
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        placeholder='Stone'
+                        onChangeText={(Weight) => setMassStone(Weight)}
+                    />) : null}
+
+                {(weightType === 'stone' ) ? (
+                    <TextInput
+                        keyboardType='numeric'
+                        style={styles.input}
+                        placeholder='Pounds'
+                        onChangeText={(Weight2) => setMassStoneLbs(Weight2)}
+                    />) : null}
+            </View>
+
+            <View>
+
+                {(weightType === 'LBS') ? (
+                <TextInput
+                    keyboardType='numeric'
+                    style={styles.input}
+                    placeholder='Pounds'
+                    onChangeText={(Weight) => setMassLBS(Weight)}
+                />) : null}
+            </View>
+
+            <View>
+
+                {weightType === "KG" ? (
+                <TextInput
+                    keyboardType='numeric'
+                    style={styles.input}
+                    placeholder='Kilograms'
+                    onChangeText={(Weight) => setMassKG(Weight)}
+                />) : null}
             </View>
 
 
-            <View style = {styles.radio}>
-                <Text>Choose your Activity level:  </Text>
+            <Text>Enter Age: </Text>
+            <TextInput
+                keyboardType='numeric'
+                style={styles.input}
+                placeholder='Age'
+                onChangeText={(Age) => setAge(Age)}
+            />
 
-                <RadioButton.Group  onValueChange={activity => setActivity(activity)} value={activity}>
-                    <RadioButton.Item label="Low Activity Levels - Less that 30 minutes a week" value="low" />
-                    <RadioButton.Item label="Average Activity Levels - Between 30-60 minutes a week" value="average" />
-                    <RadioButton.Item label="High Activity Levels - More than 60 minutes a week" value="high" />
+            <View>
+                <Text>Choose your Sex: </Text>
+
+                <RadioButton.Group onValueChange={checked => setChecked(checked)} value={checked}>
+                    <RadioButton.Item label="Male" value="first"/>
+                    <RadioButton.Item label="Female" value="second"/>
+                </RadioButton.Group>
+            </View>
+
+
+            <View style={styles.radio}>
+                <Text>Choose your Activity level: </Text>
+
+                <RadioButton.Group onValueChange={activity => setActivity(activity)} value={activity}>
+                    <RadioButton.Item label="Low Activity Levels - Less that 30 minutes a week" value="low"/>
+                    <RadioButton.Item label="Average Activity Levels - Between 30-60 minutes a week" value="average"/>
+                    <RadioButton.Item label="High Activity Levels - More than 60 minutes a week" value="high"/>
                 </RadioButton.Group>
             </View>
 
@@ -186,7 +240,8 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: 25
+        marginBottom: 25,
+        textAlign: 'center'
     },
 
 });
