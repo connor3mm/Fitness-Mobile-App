9d65b4759d135b893075c0ff9bf8267b64e00169
tamara <tamara.jankova.2020@strath.uni.ac.uk>
2022-03-17 02:59:50 +0000

    Added and displayed google maps, added a marker for location, added a search box to search for places with autocomplete but not fully working, need to add geolocation to get users current location

--- Screens/GymsNearMePage.js
+++ Screens/GymsNearMePage.js
@@ -1,14 +1,97 @@
-import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, View} from "react-native";
-import styleSheet from "react-native-web/dist/exports/StyleSheet";
+import * as React from 'react';
+import MapView, {Marker, Callout} from 'react-native-maps';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
+export default function App() {
+    const [pin, setPin] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324
+    });
 
+    const [place, setPlace] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    });
+    return (
+        <View style={{marginTop: 50, flex: 1}}>
+            <GooglePlacesAutocomplete
+                placeholder="Search"
+                fetchDetails={true}
+                GooglePlacesSearchQuery={{
+                    rankby: "distance"
+                }}
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                    setPlace({
+                        latitude: details.geometry.location.lat,
+                        longitude: details.geometry.location.lng,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    })
+                }}
+                query={{
+                    key: "AIzaSyDWtmCdM1fnzsjtw2xKsQGJBPoooW3wl8s",
+                    language: "en",
+                    components: "country:uk",
+                    types: "establishment",
+                    radius: 30000,
+                    location: `${place.latitude}, ${place.longitude}`
+                }}
+                styles={{
+                    container: {flex: 0, position: "absolute", width: "100%", zIndex: 1},
+                    listView: {backgroundColor: "white"}
+                }}
+            />
+            <MapView style={styles.map}
+                     initialRegion={{
+                         latitude: 37.78825,
+                         longitude: -122.4324,
+                         latitudeDelta: 0.0922,
+                         longitudeDelta: 0.0421
+                     }}
+                     provider="google"
+            >
+                <Marker coordinate={{latitude: place.latitude, longitude: place.longitude}}/>
+                <Marker
+                    coordinate={{
+                        latitude: 37.78825,
+                        longitude: -122.4324
+                    }}
+                    draggable={true}
+                    onDragStart={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinates)
+                    }}
+                    onDragEnd={(e) => {
+                        setPin({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude
+                        })
+                    }}
+                >
+                    <Callout>
+                        <Text>Your location</Text>
+                    </Callout>
+                </Marker>
 
-export default function GymsNearMe() {
-    return(
-        <View style = {StyleSheet.container}>
-            <Text> Gyms Near Me Screen</Text>
+            </MapView>
         </View>
-
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
