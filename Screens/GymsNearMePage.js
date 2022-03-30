eb58911bad0097d58c8ad60403ee020753a3a779
fmb20132 <cameron.pollock.2020@uni.strath.ac.uk>
2022-03-30 22:05:22 +0100

    Partial map implementation, need to add search

--- Screens/GymsNearMePage.js
+++ Screens/GymsNearMePage.js
@@ -1,38 +1,117 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image} from "react-native";
-import { styles } from './Welcomepage';
-import { setttingStyles } from './SettingsPage';
-import { caloriesStyles } from './CalorieCounterPage';
-import { BMIstyles } from './BMICalculatorPage';
-import { styling } from './Homepage';
-import CustomStatusBar from '../CustomComponents/statusBar';
-import {LinearGradient} from 'expo-linear-gradient';
+import * as React from 'react';
+import MapView, {Marker, Callout} from 'react-native-maps';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import * as Location from 'expo-location';
 
+export default function App() {
+    const [pin, setPin] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324
+    });
 
-export default function GymsNearMe({navigation}) {
+    const [region, setRegion] = React.useState({
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    });
 
-    const homePressedHandler = () => navigation.navigate('Homepage');
+    const [location, setLocation] = React.useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
 
-    return(
-        <SafeAreaView style={[styles.container]}>
-            
-            <CustomStatusBar/>
+    React.useEffect(() => {
+        (async () => {
 
-            
-            <TouchableOpacity onPress={homePressedHandler} style={{ flexDirection: 'row', alignSelf: 'flex-start'}}>
-                <Image style={{ width: 25, height: 25, marginVertical: 30, marginRight: 10,}} 
-                source={require('../assets/img/angle-left.png')}/>
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
 
-                <Text style={{ textDecorationLine: 'underline', alignSelf: 'center', 
-                fontFamily: 'Righteous_400Regular', color: '#424242', fontSize: 16.5,}}>
-                    Dashboard
-                </Text>
-            </TouchableOpacity>
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+    return (
+        <View style={{marginTop: 50, flex: 1}}>
+            <GooglePlacesAutocomplete
+                placeholder="Search"
+                fetchDetails={true}
+                GooglePlacesSearchQuery={{
+                    rankby: "distance"
+                }}
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                    setRegion({
+                        latitude: details.geometry.location.lat,
+                        longitude: details.geometry.location.lng,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    })
+                }}
+                query={{
+                    key: "AIzaSyDWtmCdM1fnzsjtw2xKsQGJBPoooW3wl8s",
+                    language: "en",
+                    components: "country:us",
+                    types: "establishment",
+                    radius: 30000,
+                    location: `${region.latitude}, ${region.longitude}`
+                }}
 
-            <Text style={[caloriesStyles.caloriesItemsText, setttingStyles.title]}>Gyms Near Me</Text>  
+                styles={{
+                    container: {flex: 0, position: "absolute", width: "100%", zIndex: 1},
+                    listView: {backgroundColor: "white"}
+                }}
 
-            
-        </SafeAreaView>
+            />
+            <MapView style={styles.map}
+                     initialRegion={{
+                         latitude: 37.78825,
+                         longitude: -122.4324,
+                         latitudeDelta: 0.0922,
+                         longitudeDelta: 0.0421
+                     }}
+                     provider="google"
+                     showUserLocation={true}
+                     region={region}
+                     onRegionChange={setRegion}
+            >
+                <Marker coordinate={{latitude: region.latitude, longitude: region.longitude}}/>
+                <Marker
+                    coordinate={pin}
+                    draggable={true}
+                    onDragStart={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinates)
+                    }}
+                    onDragEnd={(e) => {
+                        setPin({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude
+                        })
+                    }}
+                >
+                    <Callout>
+                        <Text>Your location</Text>
+                    </Callout>
+                </Marker>
 
-    )
+            </MapView>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
+});
